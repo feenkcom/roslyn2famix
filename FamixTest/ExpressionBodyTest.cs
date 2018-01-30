478e505a42c6813fc8e9f4dca1f63e5b76f8786b
George Ganea <georgeganea@gmail.com>
2018-01-30 14:59:30 +0200

    Added tests for Expression Body calls and accesses

---
+++ FamixTest/ExpressionBodyTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FamixTest
+{
+    [TestClass]
+    public class ExpressionBodyTest : SampleSystemLoader
+    {
+        [TestMethod]
+        public void ConstructorCallsAndAccesses()
+        {
+            Assert.AreEqual(2, importer.Methods.Named("SampleProject.Basic.ExpressionBody..ctor(String)").Accesses.Count);
+            Assert.AreEqual(1, importer.Methods.Named("SampleProject.Basic.ExpressionBody..ctor(String)").OutgoingInvocations.Count);
+        }
+
+        [TestMethod]
+        public void PropertySetterAccesses() => Assert.AreEqual(2, (importer.Attributes.Named("SampleProject.Basic.ExpressionBody.Name") as CSharp.CSharpProperty).setter.Accesses.Count);
+    }
+}
---
+++ SampleCode/SampleProject/Basic/ExpressionBody.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+
+namespace SampleProject.Basic
+{
+    class ExpressionBody
+    {
+
+        private static ConcurrentDictionary<int, string> names = new ConcurrentDictionary<int, string>();
+        private int id = 1;
+
+        public ExpressionBody(string name) => names.TryAdd(id, name); // constructors
+        ~ExpressionBody() => names.TryRemove(id, out _);              // finalizers
+        public string Name
+        {
+            get => names[id];                                 // getters
+            set => names[id] = value;                         // setters
+        }
+    }
+}
